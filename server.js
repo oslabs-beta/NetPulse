f80adba5e5b983ba66407087030e6cbec98dc6d6
gilessteiner1 <gilessteiner1@gmail.com>
2023-04-03 15:52:19 -0400

    fixed dependencies

--- server.js
+++ server.js
@@ -38,30 +38,49 @@ process.on('SIGTERM', () => {
 
 // --- EXPRESS SERVER / SOCKET SETUP --- //
 
+//helper function to detect string/array overlap
+const includesAny = (array, string) => {
+  for (let i = 0; i<array.length; i++){
+    if(string.includes(array[i])) return true;
+  }
+  return false
+}
+
 //custom express server running on port 4000 to send data to front end
 app.use('/',(req,res)=>{
 
   const clientData = [];
-  const spans = req.body.resourceSpans[0].scopeSpans[0].spans;
+  const spans = req.body.resourceSpans[0].scopeSpans[0].spans; 
+  const ignoreEndpoints = ['localhost','socket','nextjs']; //endpoints to ignore
 
   //add select span data to clientData array through deconstration of span elements
-  //TODO - match with typescript interface 
-  //TODO - deconstruct based on key not on array position as traces are inconsistant
+  //spans is an array of trace objects 
+  //attributes is an array of nested object with one key-value pair per array element
+  //ex: attributes = [{key: 'http.url', value: {stringValue: 'wwww.api.com/'}}...]
+  //el.attributes.find finds the array element with a matching key desired and returns the unnested value if
+  //exists or null if doesn't exist  
   spans.forEach((el)=>{
-    const clientObj = {};
-    clientObj.id = el.spanID;
-    clientObj.startTime = el.startTimeUnixNano;
-    clientObj.endTime = el.endTimeUnixNano;
-    clientObj.packageSize = el.attributes[6].value.intValue; //bytes
-    clientObj.statusCode = el.attributes[13] ? el.attributes[13].value.intValue : null;
-    clientObj.endPoint = el.attributes[0].value.stringValue;
-    clientObj.requestType = el.name;
-    clientData.push(clientObj);
-  });
+    const clientObj = {
+      spanId: el.spanId,
+      traceId: el.traceId,
+      startTime: el.startTimeUnixNano,
+      endTime: el.endTimeUnixNano,
+      packageSize: el.attributes.find(attr=>attr.key === 'http.request_content_length_uncompressed') ? el.attributes.find(attr=>attr.key === 'http.request_content_length_uncompressed').value.intValue : null,
+      statusCode: el.attributes.find(attr=>attr.key === 'http.status_code') ? el.attributes.find(attr=>attr.key === 'http.status_code').value.intValue : null,
+      endPoint: el.attributes.find(attr=>attr.key === 'http.url') ? el.attributes.find(attr=>attr.key === 'http.url').value.stringValue : null,
+      requestType: el.name
+    };
 
-  io.emit('message',JSON.stringify(clientData)); //send clientData to frontend through socket 
+    //if the endpoint is an external api add it to client data arrat
+    if(clientObj.endPoint){
+      if(!includesAny(ignoreEndpoints,clientObj.endPoint)){
+        clientData.push(clientObj); 
+      }
+    }
+  });
+  if(clientData.length > 0) io.emit('message',JSON.stringify(clientData)); //send clientData to frontend through socket 
   res.status(200).end(); //end request response cycle
-})
+});
 
 //start custom express server on port 4000
 const server = app.listen(4000, () => {
