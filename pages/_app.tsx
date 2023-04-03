2ce46ba489fd055ce1845bda5514be4902f09d1a
Thomas <eng.thomasw@gmail.com>
2023-04-03 12:31:24 -0400

    sidebar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  
+  const theme = createTheme({
+    palette: {
+      mode: 'dark',
+      primary: {
+        main: '#5a667e',
+      },
+      secondary: {
+        main: '#7e715a',
+      },
+      background: {
+        default: '#322c35',
+        paper: 'rgba(41,36,36,0.94)',
+      },
+      success: {
+        main: '#5a797e',
+      },
+    },})
+
+  return (<Component {...pageProps} />)
 }
