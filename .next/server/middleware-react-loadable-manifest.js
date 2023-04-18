0d89321318b8481562bc38759a823a338c03fb10
gilessteiner1 <gilessteiner1@gmail.com>
2023-04-18 15:31:43 -0400

    version 1.1

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST={"index.tsx -> @netpulse/dashboard":{"id":"index.tsx -> @netpulse/dashboard","files":["static/chunks/node_modules_netpulse_dashboard_dist_bundle_js.js"]}}
