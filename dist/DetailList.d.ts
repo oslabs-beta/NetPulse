e779e200b84e5f85f674e9c414d536af734ed565
gilessteiner1 <gilessteiner1@gmail.com>
2023-04-20 11:33:22 -0400

    ignored dist

--- dist/DetailList.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference types="react" />
-export default function DetailList({ columns, data }: any): JSX.Element;
--- dist/functions/errColor.d.ts
+++
@@ -1,1 +0,0 @@
-export default function errColor(contentLength: number, statusCode: number): string;
