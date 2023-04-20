ebed082e704d7e6e1206b75edca6b329f226af8f
gilessteiner1 <gilessteiner1@gmail.com>
2023-04-20 10:34:13 -0400

    deleted sidebar links

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -5,32 +5,22 @@ export default function Sidebar() {
   return (
     <>
       <div id="sidebar" className={styles.sidebar}>
-        <div className={styles.sbContent}>
+      <div className={styles.sbContent}>
           <div className={styles.mainLogo}>
             <div>
               <span className={styles.Data}>Net</span>
               <span>Pulse</span>
             </div>
-            <img 
-              src="/images/black.png"
-              className={styles.sbLogo} 
+            <a href="https://www.netpulse.dev"> </a>
+            <img
+              src="/images/netpulseicon.png"
+              className={styles.sbLogo}
               alt="DataTrace Logo"
               width="190"
               height="190"
             />
           </div>
-          <div className={styles.textLinks}>
-            <a href="/datatrace-splash" className={styles.sbLinks} >
-              Main
-            </a>
-            <a href="/about" className={styles.sbLinks} >
-              About
-            </a>
-            <a href="/blog" className={styles.sbLinks} >
-              Blog
-            </a>
-          </div>
-          <div className={styles.logoLinks} >
+          <div className={styles.logoLinks}>
             <a href="https://www.medium.com">
               <img
                 alt="medium icon"
@@ -38,19 +28,19 @@ export default function Sidebar() {
                 className={styles.mediumLogo}
               />
             </a>
-            <a href="https://www.npmjs.com">
+            <a href="https://www.npmjs.com/package/@netpulse/dashboard?activeTab=readme">
               <img
                 alt="npm icon"
                 src="https://andrejgajdos.com/wp-content/uploads/2019/11/npm-logo.png?x24361"
-                className={styles.npmLogo} 
+                className={styles.npmLogo}
               />
             </a>
-            <a href="https://www.github.com">
+            <a href="https://github.com/oslabs-beta/NetPulse/">
               <img
                 alt="github icon"
                 src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pngarts.com%2Ffiles%2F8%2FBlack-Github-Logo-PNG-Image.png&f=1&nofb=1&ipt=5c5906974e2cddb181d36fd5edbce6b3ae5528f9e5ee595828a539ded9f52efb&ipo=images"
                 className={styles.githubLogo}
-            />
+              />
             </a>
           </div>
         </div>
