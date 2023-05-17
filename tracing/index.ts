a395db5b76e4a3438342c01988472356454c9fef
code <code0312@gmail.com>
2023-05-18 06:03:33 +1000

    removed dist from git history; added tsconfig to tracig folder to turn off isolatedModules for that directory while leaving the global value set to true (required for SWC)

--- tracing/index.ts
+++ tracing/index.ts
@@ -1,6 +1,12 @@
+/*
+    A tsconfig was added to this directory to turn off isolatedModules for this directory
+    while leaving the global value of isolatedModules set to true. This is necessary as we
+    are using SWC which requires isolatedModules.k
+*/
+
 const provider = require('./instrumentation');
 
 provider.register();
 
 require('./tracingServer');
-require('./mongooseSetup');
+// require('./mongooseSetup');
